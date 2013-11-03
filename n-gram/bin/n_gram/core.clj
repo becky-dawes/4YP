6cad79468950ead4099db0cefea2e99354cbb67d
Becky Dawes <rebecca.dawes@bnc.ox.ac.uk>
2013-11-03 20:10:41 +0000

    Updates to n-gram code

--- n-gram/bin/n_gram/core.clj
+++ n-gram/bin/n_gram/core.clj
@@ -1,16 +1,17 @@
 (ns n-gram.core)
 
 
-
 ; read in file
 
 (def txt (slurp "the-wonderful-wizard-of-oz.txt"))
 
+;Remove all punctuation (except apostrophes) and convert to lower case
 
+(def formattedText (clojure.string/lower-case (clojure.string/replace txt #"[\p{P}&&[^'][\n]]" " ")))
 
 ; split tokens at whitespace (reg. expr.)
 
-(def words (clojure.string/split txt #"\s"))
+(def words (clojure.string/split formattedText #"\s+"))
 
 
 
@@ -23,15 +24,6 @@
 ; normalize word frequencies by total count
 
 (def nh (zipmap (map key h) (map #(/ % (count words)) (map val h))))
-
-
-
-; sort types (ascending) by token count
-
-(sort-by second nh)
-
-
-
 
 
 (defn cumsum
@@ -48,7 +40,7 @@
 
              (cons y ( lazy-seq (cumsum y (rest x))))))))
 
-
+; Cumulative sum of all words
 
 (def ai  (cumsum (repeat (- (count words) 1) 1)))
 
@@ -62,18 +54,70 @@
 
 
 
-(def wp (interleave  (map #(nth words (- % 1)) ai) (map #(nth words %) ai)))
+;(def wp (interleave  (map #(nth words (- % 1)) ai) (map #(nth words %) ai)))
 
 
 
-(defn pairs [x y] 
+;(defn pairs [x y] 
 
- (if (not= (count x) 0) 
+ ;(if (not= (count x) 0) 
 
-   (cons (list (first x) (first y)) (lazy-seq (pairs (rest x) (rest y))))
+  ; (cons (list (first x) (first y)) (lazy-seq (pairs (rest x) (rest y))))
 
-   (list)))
+   ;(list)))
+
+(defn makePairs "Creates a sequence of all the word pairs from the input"
+  ([theWords] (if (> (count theWords) 2)
+     (cons(take 2 theWords)(lazy-seq(makePairs (rest theWords)))))))
+
+
+;(def pairFreq (frequencies (pairs (map #(nth words (- % 1)) ai) (map #(nth words %) ai))))
+
+; Create sequence of all word pairs
+
+(def pairs (makePairs words))
+
+; Find frequency of each word pair
+
+(def pairFreq (frequencies pairs))
+
+; Normalise pair frequencies
+
+(def pairFreqNorm (zipmap (map key pairFreq) (map #(/ % (count pairs)) (map val pairFreq))))
+
+(defn p1 "Returns unigram probability of input word"
+  ([word1] (float (nh word1))))
+         
+(defn p2 "Returns bigram probability of input words" 
+  ([word1 word2] (float (/ (pairFreqNorm [word1 word2]) (nh word1)))))
+
+
+(defn makeTrios "Creates a sequence of the word trios from the input"
+  ([theWords] (if (> (count theWords) 3)
+     (cons(take 3 theWords)(lazy-seq(makeTrios (rest theWords)))))))
+
+; Create sequence of all word trios
+
+(def trios (makeTrios words))
+
+; Find frequency of each word trio
+
+(def trioFreq (frequencies trios))
+
+; Normalise trio frequencies
+
+(def trioFreqNorm (zipmap (map key trioFreq) (map #(/ % (count trios)) (map val trioFreq))))
 
 
 
-(sort-by second (frequencies (pairs (map #(nth words (- % 1)) ai) (map #(nth words %) ai))))
+(defn p3 "Returns trigram probability of input words"
+  ([word1 word2 word3] (float (/ (trioFreqNorm [word1 word2 word3]) 
+           (pairFreqNorm [word1 word2])))))
+         
+(defn p "Returns n-gram probability depending on number of input words"
+  ([word1] (p1 word1))
+  ([word1 word2] (p2 word1 word2))
+  ([word1 word2 word3] (p3 word1 word2 word3)))
+
+         
+         
